081222d884b96fd7b4bb7af90742aa34d3fcc109
Robert de Koning <robert.dekoning@ogd.nl>
2017-02-12 17:55:12 +0100

    Version1

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,17 @@
 $(function() {
-
-  // your code will go here
+     $.ajax({
+    url: 'https://www.codeschool.com/users/RdeKoning.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      response.courses.completed.forEach(function(item,index){
+          var a = $("#badges").append("div");
+          $(a).addClass("course");
+          $(a).append("<h3>"+item.title+"</h3>");
+          $(a).append("<img src="+item.badge+">");
+          $(a).append("<a href="+item.url+" target=_blank class='btn btn-primary'>'See Course'</a>");
+      })
+    }
+  });
+  
 
 });
